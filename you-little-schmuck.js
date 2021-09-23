45098d1e6d3368e37b13ab0da1dbe56e5d0672d4
Cesese <20051457+Cesese@users.noreply.github.com>
2021-09-24 00:29:47 +0200

    you-little-schmuck 0.3
    now fetches the google logo by looking for a picture with the alt "Google", and uses its parent as a box
    also, added the google favicon to the userscript

--- you-little-schmuck.js
+++ you-little-schmuck.js
@@ -1,16 +1,19 @@
 // ==UserScript==
 // @name         You Little Schmuck!
-// @version      0.1
+// @namespace    https://github.com/Cesese/UserScripts-cesese
+// @version      0.3
 // @description  Improve the google logo
 // @author       A little schmuck
 // @match        https://www.google.com/*
 // @grant        none
+// @icon         https://www.google.com/favicon.ico
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     // Your code here...
+    var text = "You Little Schmuck";
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -18,18 +21,29 @@
         return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
     }
 
-    document.getElementById("hplogo").remove(); // remove Google logo
-    var box = document.getElementById("lga");
+    function getRandomIntExcept(min, max, exc) {
+        var result = exc;
+        while ( result == exc ) {
+            result = getRandomInt(min, max);
+        }
+        return result;
+    }
+
+    var pictures = document.getElementsByTagName("IMG");
+    for (var i = 0; (i < pictures.length) && (pictures[i].alt != "Google"); i++) {}
+    var box = pictures[i].parentElement;
+    pictures[i].remove();
     var div = document.createElement('div');
-    var text = "you little schmuck";
     var btext = "";
     var colors = ["blue", "red", "yellow", "green"];
-    for (var i = 0; i < text.length; i++) { // going through text
-        var c = colors[getRandomInt(0, colors.length)]; // c is one of the colors
+    var randomNumber = 0;
+    for (i = 0; i < text.length; i++) { // going through text
+        randomNumber = getRandomIntExcept(0, colors.length, randomNumber);
+        var c = colors[randomNumber]; // c is one of the colors
         var t = '<span style="color:' + c + '">' + text[i] + '</span>' // t is the i-th letter in the text, space included
         btext = btext + t; // good letter goes into beautiful text
     }
-    div.style = "font-size: 90px; padding-top: 109px" // big font size, same padding as google's logo
+    div.style = "font-size: 90px; font-weight: bold" // big font size, same padding as google's logo //padding-top: 109px
     div.innerHTML = btext;
 
     box.appendChild(div);
